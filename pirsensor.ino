ba2834f80d1f2174fdb56a54c67031818d6a7aa6
Frigate <88032896+frigaate@users.noreply.github.com>
2023-04-01 12:18:30 +0100

    removed the pin to control perimeter light, and named the variables properly

--- pirsensor.ino
+++ pirsensor.ino
@@ -1,8 +1,7 @@
 const int pirPin = 2;  /* PIR sensor O/P pin */
 const int ledPin1 = 13;
-const int ledPin2 = 12;
-const int ledPin3 = 11;
-const int ledPin4 = 10;
+const int fanPin3 = 11;
+const int acPin4 = 10;
 int warm_up;
 unsigned long previousMillis = 0;
 const long interval = 30000;  // interval at which to read the sensor (in milliseconds)
@@ -10,9 +9,8 @@ const long interval = 30000;  // interval at which to read the sensor (in millis
 void setup() {
   pinMode(pirPin, INPUT);
   pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
-  pinMode(ledPin3, OUTPUT);
-  pinMode(ledPin4, OUTPUT);
+  pinMode(fanPin3, OUTPUT);
+  pinMode(acPin4, OUTPUT);
   Serial.begin(9600); /* Define baud rate for serial communication */
 }
 
@@ -27,16 +25,14 @@ void loop() {
         Serial.print("Warming Up\n\n");
         warm_up = 0;
         digitalWrite(ledPin1, LOW);
-        digitalWrite(ledPin2, LOW);
-        digitalWrite(ledPin3, LOW);
-        digitalWrite(ledPin4, LOW);
+        digitalWrite(fanPin3, LOW);
+        digitalWrite(acPin4, LOW);
       }
       else {
         Serial.print("Object detected\n\n");    
         digitalWrite(ledPin1, HIGH);
-        digitalWrite(ledPin2, HIGH);
-        digitalWrite(ledPin3, HIGH);
-        digitalWrite(ledPin4, HIGH);
+        digitalWrite(fanPin3, HIGH);
+        digitalWrite(acPin4, HIGH);
       }
     }
 }
